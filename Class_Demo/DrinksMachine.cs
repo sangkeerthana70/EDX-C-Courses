114229eb3d2bc96782cfdda7242c2130111ddadd
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-05-15 14:20:17 -0400

    Module-1 class demo

---
+++ Class_Demo/DrinksMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Demo
+{
+    class DrinksMachine
+    {
+        //the following statements declare private member variables
+        public string _location;
+        public string _make;
+        public string _model;
+        //the following statements declare public methods
+        public void MakeCappuccino()
+        {
+            // Method logic goes here.
+        }
+        public void MakeEspresso()
+        {
+            // Method logic goes here.
+        }
+
+
+    }
+}
---
+++ Class_Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Class_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine();
+            myMachine._location = "Kitchen";
+            myMachine._model = "DM1000";
+
+            Console.WriteLine(myMachine._location);
+
+        }
+    }
+}
