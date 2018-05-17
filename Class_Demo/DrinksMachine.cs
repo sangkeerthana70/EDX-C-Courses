f212fe5697aa2f14eb2fcad42e9229da7a932b7d
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-05-17 13:01:40 -0400

    Encapsulation demo done

--- Class_Demo/DrinksMachine.cs
+++ Class_Demo/DrinksMachine.cs
@@ -6,14 +6,28 @@ namespace Class_Demo
 {
     class DrinksMachine
     {
+        //another constructor to assign member variables when we create a new Drinks machine object
+        public DrinksMachine(string loc, string make, string model)
+        {
+            this.Location = loc;
+            this.Make = make;
+            this.Model = model;
+        }
         //the following statements declare private member variables
-        public string _location;
-        public string _make;
-        public string _model;
+        private string _location;
+        private string _make;
+        private string _model;
+
+        //the following statements are public access  methods
+        public string Location { get => _location; set => _location = value; }
+        public string Make { get => _make; set => _make = value; }
+        public string Model { get => _model; set => _model = value; }
+
+
         //the following statements declare public methods
         public void MakeCappuccino()
         {
-            // Method logic goes here.
+            Console.WriteLine("Cappuccino is made");
         }
         public void MakeEspresso()
         {
