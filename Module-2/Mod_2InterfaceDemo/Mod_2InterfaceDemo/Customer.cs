b174decc21a1505582cef403d59c8cfbe0bcef4c
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-05-31 15:27:34 -0400

    Completed Interface demo

--- Module-2/Mod_2InterfaceDemo/Mod_2InterfaceDemo/Customer.cs
+++ Module-2/Mod_2InterfaceDemo/Mod_2InterfaceDemo/Customer.cs
@@ -9,12 +9,28 @@ namespace Mod_2InterfaceDemo
     //implement ILoyaltyCardHolder interface
     class Customer : ILoyaltyCardHolder
     {
-        //implementation code
-        private int totalPoints;
 
-        public int TotalPoints
+        //implementation code
+        private int totalPoints;//private member variables
+
+        public int TotalPoints//property
         {
             get { return totalPoints; }
         }
+
+
+        public int AddPoints(decimal transactionValue)
+
+        {
+            int points = Decimal.ToInt32(transactionValue);
+            totalPoints += points;
+            return totalPoints;
+
+            public void ResetPoints()
+            {
+                totalPoints = 0;
+            }
+        }
+
     }
 }
