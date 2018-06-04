c5acb1eb694fa4bcde22433576993ac8fc2999bb
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-04 00:14:17 -0400

    Memory management Demo implemented

---
+++ Module-3/Mod3GCDemo/Mod3GCDemo/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3GCDemo
+{
+    class Animal
+    {
+            public int AnimalID { get; set; }
+
+            public string Species { get; set; }
+
+            public string Habitat { get; set; }
+        
+    }
+}
---
+++ Module-3/Mod3GCDemo/Mod3GCDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3GCDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
